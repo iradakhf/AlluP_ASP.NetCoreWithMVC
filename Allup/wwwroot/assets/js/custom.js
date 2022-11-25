dc7894555b91f367085ce10fbea4429cd27610e9
iradakhf <iradakhf@code.edu.az>
2022-11-25 11:25:05 +0400

    added some changes and jow modal works

--- Allup/wwwroot/assets/js/custom.js
+++ Allup/wwwroot/assets/js/custom.js
@@ -1,28 +1,29 @@
 ﻿$(document).ready(() => {
-    $(".searchBtn").click(() => {
-        let searchInput = $(".searchInput").val();
-    let selectedCategory = $(".searchCategory option:selected").val();
+    //$(".searchBtn").click(() => {
+    //    let searchInput = $(".searchInput").val();
+    //let selectedCategory = $(".searchCategory option:selected").val();
 
 
-        fetch("/shop/search/" + selectedCategory + "?text=" + searchInput)
-            .then(response => {
-                return response.text();
-            }).then(data => {
-                $("#searchResult").html(data);
+    //    fetch("/shop/search/" + selectedCategory + "?text=" + searchInput)
+    //        .then(response => {
+    //            return response.text();
+    //        }).then(data => {
+    //            $("#searchResult").html(data);
 
-            })
-        $("#listForSearch").html(data);
-    })
-    $("#modalIcon").click((e) => {
-        e.preventDefault(e);
-        let href = $("#modalIcon").attr("href");
-        fetch(href)
-            .then(response => {
-                return response.text();
-            }).then(data => {
-                $(".modal-content").html(data);
-            })
-        $("#productQuickModal").modal("show");
+    //    $("#listForSearch").html(data);
+    //        })
+    //})
+    $(document).on("click", "#modalIcon", function (e) {
+        e.preventDefault()
 
+        let url = $(this).attr("href")
+        
+        fetch(url).then(response => 
+             response.text()
+        ).then(data => {
+            $(".modal-body").html(data)
+            $(".modal-body").modal("show")
+
+        })
     })
 })
