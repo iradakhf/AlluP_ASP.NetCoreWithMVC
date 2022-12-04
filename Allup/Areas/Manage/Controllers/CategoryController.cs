933ddfbd773a80307f64433baa4a92f3b20efbc6
iradakhf <iradakhf@code.edu.az>
2022-12-04 19:04:20 +0400

    added update part in admin panel

--- Allup/Areas/Manage/Controllers/CategoryController.cs
+++ Allup/Areas/Manage/Controllers/CategoryController.cs
@@ -66,7 +66,7 @@ namespace Allup.Areas.Manage.Controllers
                 }
                 string originalFileName = category.File.FileName.Substring(category.File.FileName.LastIndexOf("."), 4);
                 string fileName = Guid.NewGuid().ToString() + "-" + DateTime.UtcNow.AddHours(4).ToString("yyyymmddhhmmss") + originalFileName;
-                string path = @"C:\Users\irade\source\repos\AlluP_ASP.NetCoreWithMVC\Allup\wwwroot\assets\images\" + fileName;
+                string path = @"C:\Users\irade\source\repos\new\AlluP_ASP.NetCoreWithMVC\Allup\wwwroot\assets\images\" + fileName;
                 using (FileStream fileStream = new FileStream(path, FileMode.Create))
                 {
                     await category.File.CopyToAsync(fileStream);
@@ -94,6 +94,95 @@ namespace Allup.Areas.Manage.Controllers
             category.CreatedAt = DateTime.UtcNow.AddHours(4);
             category.CreatedBy = "System";
             await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+       
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id==null)
+            {
+                return BadRequest("id can not be null");
+            }
+            Category category = await _context.Categories.FirstOrDefaultAsync(C => C.IsDeleted == false && C.Id == id);
+            if (category==null)
+            {
+                return NotFound("can not find category with this id");
+            }
+            ViewBag.Categories = await _context.Categories
+                .Where(c => c.IsDeleted == false && c.IsMain)
+                .ToListAsync();
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Category category)
+        {
+            ViewBag.Categories = await _context.Categories
+             .Where(c => c.IsDeleted == false && c.IsMain)
+             .ToListAsync();
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            Category existedCategory = await _context.Categories.FirstOrDefaultAsync(C => C.IsDeleted == false && C.Id == id);
+            if (existedCategory == null)
+            {
+                return NotFound("can not find category with this id");
+            }
+            if (category.Id != id)
+            {
+                return BadRequest("id can not be null");
+
+            }
+            if (category.IsMain)
+            {
+                category.ParentId = null;
+                if (category.File == null)
+                {
+                    ModelState.AddModelError("File", "is required");
+                    return View();
+                }
+                if (category.File.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("File", "file type should be jpeg or jpg");
+                    return View();
+                }
+                if (category.File.Length > 20000)
+                {
+                    ModelState.AddModelError("File", "file length should be less than 20k");
+                    return View();
+                }
+                string originalFileName = category.File.FileName.Substring(category.File.FileName.LastIndexOf("."), 4);
+                string fileName = Guid.NewGuid().ToString() + "-" + DateTime.UtcNow.AddHours(4).ToString("yyyymmddhhmmss") + originalFileName;
+                string path = @"C:\Users\irade\source\repos\new\AlluP_ASP.NetCoreWithMVC\Allup\wwwroot\assets\images\" + fileName;
+                using (FileStream fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await category.File.CopyToAsync(fileStream);
+                }
+                existedCategory.ParentId = null;
+                existedCategory.Image = fileName;
+
+            }
+            else
+            {
+                if (category.ParentId == null)
+                {
+                    ModelState.AddModelError("ParenId", "parent should be choosen");
+                    return View(category);
+                }
+                if (!await _context.Categories.AnyAsync(c => c.IsDeleted == false && c.IsMain && c.Id == category.ParentId))
+                {
+                    ModelState.AddModelError("ParentId", "choose the vorrect parent category");
+                    return View(category);
+                }
+                existedCategory.Image = null;
+            }
+            existedCategory.IsMain = category.IsMain;
+            existedCategory.Name = category.Name;
+            existedCategory.UpdatedAt = DateTime.UtcNow.AddHours(4);
+            existedCategory.UpdatedBy = "System";
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
